95f752b322b23be2c97b005cf94af28c5b2ee829
enerGneer <energneer@gmail.com>
2023-05-21 06:30:40 +0900

    Add template literals

---
+++ JS-start/05-template-literals.js
@@ -0,0 +1,10 @@
+// 오늘 날짜 예쁘게 보여주기
+console.log("오늘은 " + year + "년 " + month + "월 " + date + "일 입니다.");
+// 오늘은 2021년 4월 3일 입니다.
+
+// 지금 시간 예쁘게 보여주기
+console.log("지금은 " + hours + "시 " + minutes + "분 " + seconds + "초 입니다.");
+// 지금은 13시 30분 17초 입니다.
+
+console.log(`오늘은 ${year}년 ${month}월 ${date}일 입니다.`);
+console.log(`오늘은 ${hours}시 ${minutes}분 ${seconds}초 입니다.`);
