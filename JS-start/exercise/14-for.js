d4941e494ac38fb8cdd89a0a0cf33df8c2d22325
enerGneer <energneer@gmail.com>
2023-06-24 06:51:29 +0900

    Add for js

---
+++ JS-start/exercise/14-for.js
@@ -0,0 +1,17 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let el of arr) {
+  console.log(el);
+}
+
+// for in
+const obj = {
+  name: "otter",
+  gender: "male",
+};
+
+for (let key in obj) {
+  console.log(key);
+}
