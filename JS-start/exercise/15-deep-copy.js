d1c130844509cd2daaaccebc99838b01a43ed0a1
enerGneer <energneer@gmail.com>
2023-06-28 06:53:38 +0900

    Update deep copy js

--- JS-start/exercise/15-deep-copy.js
+++ JS-start/exercise/15-deep-copy.js
@@ -11,3 +11,19 @@ const copy = JSON.stringify(obj);
 
 console.log(copy);
 // {"name":"otter","gender":"male","favoriteFood":{"first":"sushi","second":"hamburger"}}
+
+const deepCopy = JSON.parse(copy);
+
+console.log(deepCopy);
+
+/*
+	{
+		name: "otter",
+		gender: "male",
+	  favoriteFood: {
+			first: "sushi",
+			second: "hamburger"
+	  }
+	}
+*/
+// 원본 객체와는 전혀 다른 주소값을 가진 객체가 생성
