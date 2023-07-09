37ab5b90ba2a537c24b232430a7e9210424b73aa
enerGneer <energneer@gmail.com>
2023-07-09 10:18:36 +0900

    Add invoke js

---
+++ JS-exercise/05_invoke.js
@@ -0,0 +1,10 @@
+function invoke() {
+  // double이라는 함수를 호출하고, 첫번째 인자로 숫자 7을 넣은 뒤, 결과값을 result에 할당하세요
+  let result;
+  // 여기에 코드를 작성하세요
+
+  result = double(7);
+
+  //아래 코드는 변경하지 마세요
+  return result;
+}
