959e94e14655d9c6243940f1db0b39b640bdad7d
enerGneer <energneer@gmail.com>
2023-07-12 06:55:25 +0900

    Add string

---
+++ JS-exercise/07_addstring.js
@@ -0,0 +1,10 @@
+function addString() {
+  let result;
+  // 문자 7과 숫자 1을 더한 결과를 result 변수에 더해주세요.
+  // 여기에 코드를 작성하세요.
+  result = "7" + 1;
+  // 아래 코드는 수정하지 마세요.
+  return result;
+}
+
+addString();
