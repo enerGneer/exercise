e5af65808401a2b6559123362bd209a64ba709b6
enerGneer <energneer@gmail.com>
2023-07-16 10:37:18 +0900

    Add levelfilter

---
+++ JS-exercise/2/01_levelfilter.js
@@ -0,0 +1,11 @@
+function levelFilter(level) {
+  // 여기에 코드를 작성하세요.
+  if (level >= 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+levelFilter(40);
+levelFilter(30);
+levelFilter(29);
