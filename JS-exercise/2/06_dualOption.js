b9780ae3ea012a56b9f3a529070b9e567f2074b0
enerGneer <energneer@gmail.com>
2023-07-22 06:33:14 +0900

    Add dualoption

---
+++ JS-exercise/2/06_dualOption.js
@@ -0,0 +1,12 @@
+function dualOption(num) {
+  // 여기에 코드를 작성하세요.
+  if (num >= 30 && num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+dualOption(32);
+dualOption(31);
+dualOption(28);
