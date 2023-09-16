6a8d634d04f9d208cf2221119ac4f180c148b55d
enerGneer <energneer@gmail.com>
2023-09-17 07:24:55 +0900

    Add typeof

---
+++ JS-exer-Algorithm/1/13.js
@@ -0,0 +1,18 @@
+// 주어진 변수 str과 number의 데이터 타입을 확인하세요.
+
+// **`입력 인자`**
+
+// - X
+
+// **`주의 사항`**
+
+// - 문자열과 숫자열은 어떻게 다른지 확인해 보세요.
+
+let str = "3";
+let number = 3;
+
+typeof str;
+typeof number;
+
+console.log(typeof str); // string
+console.log(typeof number); // number
