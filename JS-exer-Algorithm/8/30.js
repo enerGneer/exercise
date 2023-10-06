8c35e4f5b02b5db3e4bbaa0df9f155995655f1bb
enerGneer <energneer@gmail.com>
2023-10-07 08:29:31 +0900

    Update pass check

--- JS-exer-Algorithm/8/30.js
+++ JS-exer-Algorithm/8/30.js
@@ -32,10 +32,10 @@ let password1 = "a1B2c3D4";
 let password2 = "A1b2C3d4";
 validation(password1, password2); // true
 
-let password1 = "1234";
-let password2 = "1234";
-validation(password1, password2); // "비밀번호는 8~16자리여야 합니다"
+let password3 = "1234";
+let password4 = "1234";
+validation(password3, password4); // "비밀번호는 8~16자리여야 합니다"
 
-let password1 = "12345678";
-let password2 = "12345679";
-validation(password1, password2); // "비밀번호를 다시 확인해주세요"
+let password5 = "12345678";
+let password6 = "12345679";
+validation(password5, password6); // "비밀번호를 다시 확인해주세요"
