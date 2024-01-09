1fdb230e214f375688a83941d180f8c13a9b591e
enerGneer <energneer@gmail.com>
2024-01-09 18:14:22 +0900

    Update query parameters

--- Node-project/routes/restaurants.js
+++ Node-project/routes/restaurants.js
@@ -5,12 +5,34 @@ const resData = require('../util/restaurant-data');
 
 const router = express.Router();
 
-router.get('/restaurants', function (req, res) { 
+router.get('/restaurants', function (req, res) {
+  let order = req.query.order;
+  let nextOrder = 'desc';
+
+  if (order !== 'asc' && order !== 'desc') {
+    order = 'asc';
+  }
+
+  if (order === 'desc') {
+    nextOrder = 'asc';
+  }
+
   const storedRestaurants = resData.getStoredRestaurants();
+
+  storedRestaurants.sort(function (resA, resB) {
+    if (
+      (order === 'asc' && resA.name > resB.name) ||
+      (order === 'desc' && resB.name > resA.name)
+    ) {
+      return 1;
+    }
+    return -1;
+  });
 
   res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
+    nextOrder: nextOrder
   });
 });
 
@@ -24,10 +46,10 @@ router.get('/restaurants/:id', function (req, res) {
     }
   }
 
-  res.render('404');
+  res.status(404).render('404');
 });
 
-router.get('/recommend', function (req, res) { 
+router.get('/recommend', function (req, res) {
   res.render('recommend');
 });
 
